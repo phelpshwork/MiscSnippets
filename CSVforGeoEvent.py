665e8019d589d024dda6ec1acbc61d1b5eef5951
Heather Phelps <30875412+phelpshwork@users.noreply.github.com>
2018-06-04 13:17:27 -0400

    Update CSVforGeoEvent.py
    Modifications because the first effort wouldn't absorb into GeoEvent

--- CSVforGeoEvent.py
+++ CSVforGeoEvent.py
@@ -2,19 +2,17 @@
 # code to convert a CSV file to GeoEvent-readable file
 
 import csv
+import string
 #inpath = r'C:\temp\temp\locatext_out.csv'
 inpath = r'C:\temp\temp\sigs.csv'
 outpath = r'C:\temp\temp\out' 
 infile = open(inpath, 'r', encoding='utf-8-sig', errors='ignore') 
 
-# with infile as f:
-#     reader = csv.reader(f)
-#     for row in reader:
-#         print(row[0])
-
-with infile, open(outpath + r'\sigs_new.csv', 'w', encoding='utf-8-sig') as outfile:
+with infile, open(outpath + r'\sigs_new_with_join_strip.csv', 'w', encoding='utf-8-sig') as outfile:
     text = infile.read()
-    # process Unicode text
-    outfile.write(text)
-
+    printable = set(string.printable)
+    p = ''.join(filter(lambda x: x in string.printable, text))
+    outfile.write(p)
+    
 print("Success!")
+#opens file as UTF-8 - it thinks it's a unicode file but it's not. Need to write another file using UTF-8 for GeoEvent
